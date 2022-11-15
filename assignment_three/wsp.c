e6cc02ab81a18a228bfed188474aed13555649ec
aliazak6 <aliazak6@gmail.com>
2022-11-15 12:30:20 +0300

    sequential version

--- assignment_three/wsp.c
+++ assignment_three/wsp.c
@@ -69,135 +69,144 @@ int *DIST = NULL; // one dimensional array used as a matrix of size (NCITIES * N
 
 
 typedef struct path_struct_t {
-  int cost;         // path cost.
-  city_t *path;     // order of city visits (you may start from any city).
+	int cost;         // path cost.
+	city_t *path;     // order of city visits (you may start from any city).
 } path_t;
 path_t *bestPath = NULL;
 
 
 // set DIST[i,j] to value
 inline static void set_dist(int i, int j, int value) {
-  assert(value > 0);
-  int offset = i * NCITIES + j;
-  DIST[offset] = value;
-  return;
+	assert(value > 0);
+	int offset = i * NCITIES + j;
+	DIST[offset] = value;
+	return;
 }
 
 // returns value at DIST[i,j]
 inline static int get_dist(int i, int j) {
-  int offset = i * NCITIES + j;
-  return DIST[offset];
+	int offset = i * NCITIES + j;
+	return DIST[offset];
 }
 
 // prints results
 void wsp_print_result() {
-  printf("========== Solution ==========\n");
-  printf("Cost: %d\n", bestPath->cost);
-  printf("Path: ");
-  for(int i = 0; i < NCITIES; i++) {
-    if(i == NCITIES-1) printf("%d", bestPath->path[i]);
-    else printf("%d -> ", bestPath->path[i]);
-  }
-  putchar('\n');
-  putchar('\n');
-  return;
+	printf("========== Solution ==========\n");
+	printf("Cost: %d\n", bestPath->cost);
+	printf("Path: ");
+	for(int i = 0; i < NCITIES; i++) {
+		if(i == NCITIES-1) printf("%d", bestPath->path[i]);
+		else printf("%d -> ", bestPath->path[i]);
+	}
+	putchar('\n');
+	putchar('\n');
+	return;
 }
 
 
-void wsp_iterate(path_t* currentPath,int city_count) {
-  int skip_flag = 0;
-  int cost = 0;
-  for(int i=0; i<NCITIES; i++) { // loop to iterate over different cities
-    skip_flag = 0;
-    cost = 0;
-    for(int j=0; j<city_count; j++) { // loop to iterate over cities in current path
-      if(currentPath->path[j] == i) { // if city is already in path, skip it
-        skip_flag = 1;
-        break;
-      }
-    }
-    if(skip_flag == 1) { // if city is already in path, skip it
-      continue;
-    }
-    currentPath->path[city_count] = i;
-    cost = get_dist(currentPath->path[i-1],currentPath->path[i]);
-    if(cost + currentPath->cost <= bestPath->cost){
-      currentPath->cost += cost;
-      wsp_iterate(currentPath, city_count+1);  
-    } 
-    if(city_count == NCITIES) {
-      if(currentPath->cost < bestPath->cost) {
-        bestPath->cost = currentPath->cost;
-        for(int i=0; i<NCITIES; i++) {
-          bestPath->path[i] = currentPath->path[i];
-        }
-      }
-    } 
-  }
-}
+path_t wsp_iterate(path_t currentPath,int city_count) {
+	int skip_flag = 0;
+	int cost = 0;
+	for(int i=0; i<NCITIES; i++) { // loop to iterate over different cities
+		/* PART WE DECIDE TO ITERATE OR NOT*/
+		skip_flag = 0;
+		cost = 0;
+
+		for(int j=0; j<city_count; j++) { // loop to iterate over cities in current path
+			if(currentPath.path[j] == i) { // if city is already in path, skip it
+				skip_flag = 1;
+				break;
+			}
+		}
+		if(skip_flag == 1) { // if city is already in path, skip it
+			continue;
+		}
+		/*********************************/
+		/* PART WE ITERATE OVER*/
+		currentPath.cost = 0;
+		for(int k = 0; k<city_count-1; k++) { // loop to iterate over cities in current path
+			currentPath.cost += get_dist(currentPath.path[k],currentPath.path[k+1]);
+		}
+		cost = currentPath.cost + get_dist(currentPath.path[city_count-1],i); // add cost of current city to path cost
+		if(cost > bestPath->cost){
+			continue;
+		}else{
+			currentPath.path[city_count] = i;
+			currentPath.cost = cost;
+			if(city_count == NCITIES-1) { // if we have reached the end of the path
+				if(cost < bestPath->cost) { // if the cost of the path is less than the best path
+					bestPath->cost = cost;
+					for(int j=0; j<NCITIES; j++) {
+						bestPath->path[j] = currentPath.path[j];
+					}
+				}			
+			}else{
+				currentPath = wsp_iterate(currentPath,city_count+1);
+			}
+		}
+		/*********************************/		
+	}
+	currentPath.cost-= get_dist(currentPath.path[city_count-1],currentPath.path[city_count]);
+	return currentPath;
+}	
 
 void wsp_start() {
 
-  // TODO: try finding a better path.
-  int cityID = 0;
-  for(cityID=0; cityID < NCITIES; cityID++) {
-    bestPath->path[cityID] = cityID;
-    if(cityID>0) bestPath->cost += get_dist(bestPath->path[cityID-1],bestPath->path[cityID]);
-  }
-  int root,city_count;
-  path_t* currentPath = (path_t*)malloc(sizeof(path_t));
-  currentPath->cost = 0;
-  currentPath->path = (city_t*)calloc(NCITIES, sizeof(city_t));
-  for(cityID=0; cityID < NCITIES; cityID++) { // loop to iterate over different root nodes
-    root = cityID; // private
-    currentPath->path[0] = root; //private
-    city_count = 1; // private
-    wsp_iterate(currentPath,city_count);
-  }
+	// TODO: try finding a better path.
+	int cityID = 0;
+	for(cityID=0; cityID < NCITIES; cityID++) {
+		bestPath->path[cityID] = cityID;
+		if(cityID>0) bestPath->cost += get_dist(bestPath->path[cityID-1],bestPath->path[cityID]);
+	}
+	int root,city_count;
+	path_t currentPath ;
+	currentPath.path = (city_t*)calloc(NCITIES, sizeof(city_t));
+	for(cityID=0; cityID < NCITIES; cityID++) { // loop to iterate over different root nodes
+		root = cityID; // private
+		currentPath.path[0] = root; //private
+		currentPath.cost = 0; //private
+		city_count = 1; // private
+		wsp_iterate(currentPath,city_count);
+	}
 
-
-  return;
 }
 
 int main(int argc, char **argv) {
-  for(int i = 0; i < argc; i++) {
-    printf("argv[%d] = %s\n", i, argv[i]);
-  }
-  if(argc < 4 || strcmp(argv[1], "-p") != 0) error_exit("Expecting two arguments: -p [processor count] and [file name]\n");
-  NCORES = atoi(argv[2]);
-  if(NCORES < 1) error_exit("Illegal core count: %d\n", NCORES);
-  char *filename = argv[3];
-  FILE *fp = fopen(filename, "r");
-  if(fp == NULL) error_exit("Failed to open input file \"%s\"\n", filename);
-  int scan_ret;
-  scan_ret = fscanf(fp, "%d", &NCITIES);
-  if(scan_ret != 1) error_exit("Failed to read city count\n");
-  if(NCITIES < 2) {
-    error_exit("Illegal city count: %d\n", NCITIES);
-  } 
-  // Allocate memory and read the matrix
-  DIST = (int*)calloc(NCITIES * NCITIES, sizeof(int));
-  SYSEXPECT(DIST != NULL);
-  for(int i = 1;i < NCITIES;i++) {
-    for(int j = 0;j < i;j++) {
-      int t;
-      scan_ret = fscanf(fp, "%d", &t);
-      if(scan_ret != 1) error_exit("Failed to read dist(%d, %d)\n", i, j);
-      set_dist(i, j, t);
-      set_dist(j, i, t);
-    }
-  }
-  fclose(fp);
-  bestPath = (path_t*)malloc(sizeof(path_t));
-  bestPath->cost = 0;
-  bestPath->path = (city_t*)calloc(NCITIES, sizeof(city_t));
-  double before = omp_get_wtime();
-  wsp_start();
-  double after = omp_get_wtime();
-  double delta_ms = (after - before) * 1000.0 + (after - before) / 1000000.0;
-  putchar('\n');
-  printf("============ Time ============\n");
-  printf("Time: %.3f ms (%.3f s)\n", delta_ms, delta_ms / 1000.0);
-  wsp_print_result();
-  return 0;
+	if(argc < 4 || strcmp(argv[1], "-p") != 0) error_exit("Expecting two arguments: -p [processor count] and [file name]\n");
+	NCORES = atoi(argv[2]);
+	if(NCORES < 1) error_exit("Illegal core count: %d\n", NCORES);
+	char *filename = argv[3];
+	FILE *fp = fopen(filename, "r");
+	if(fp == NULL) error_exit("Failed to open input file \"%s\"\n", filename);
+	int scan_ret;
+	scan_ret = fscanf(fp, "%d", &NCITIES);
+	if(scan_ret != 1) error_exit("Failed to read city count\n");
+	if(NCITIES < 2) {
+		error_exit("Illegal city count: %d\n", NCITIES);
+	} 
+	// Allocate memory and read the matrix
+	DIST = (int*)calloc(NCITIES * NCITIES, sizeof(int));
+	SYSEXPECT(DIST != NULL);
+	for(int i = 1;i < NCITIES;i++) {
+		for(int j = 0;j < i;j++) {
+			int t;
+			scan_ret = fscanf(fp, "%d", &t);
+			if(scan_ret != 1) error_exit("Failed to read dist(%d, %d)\n", i, j);
+			set_dist(i, j, t);
+			set_dist(j, i, t);
+		}
+	}
+	fclose(fp);
+	bestPath = (path_t*)malloc(sizeof(path_t));
+	bestPath->cost = 0;
+	bestPath->path = (city_t*)calloc(NCITIES, sizeof(city_t));
+	double before = omp_get_wtime();
+	wsp_start();
+	double after = omp_get_wtime();
+	double delta_ms = (after - before) * 1000.0 + (after - before) / 1000000.0;
+	putchar('\n');
+	printf("============ Time ============\n");
+	printf("Time: %.3f ms (%.3f s)\n", delta_ms, delta_ms / 1000.0);
+	wsp_print_result();
+	return 0;
 }
